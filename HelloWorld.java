d4ee74a3e4c560675831f2ae97fed8662f0f38af
nikitaramtri <nikitaramtri@gmail.com>
2020-09-16 09:01:36 +0000

    experimenting branches

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 class HelloWorld{
 	public static void main(String[] args){
-		for(int i=0;i<8;i++){
+		for(int i=0;i<5;i++){
 			System.out.println("Hello World");
+			System.out.println("Welcome");
 		}
 	}
 }
